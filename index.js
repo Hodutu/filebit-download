df1d02cd9095485bead89bbfd9de026704ef45eb
Michał Budzyński <michal@virtualdesign.pl>
2014-11-29 01:01:00 +0100

    :alien: proper header parsing

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+var request = require('request');
 var link = require('./link.json').url;
 
 console.log(link);
+request.head(link, function (error, response, body) {
+  console.log(response.headers['content-length']);
+  if (!error && response.statusCode == 200) {
+    console.log(body);
+  }
+})
